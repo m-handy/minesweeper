e4a76d9e9916d9f5af1cb17fb87217411ce5a2f7
Martin Hanyáš <28785549+m-handy@users.noreply.github.com>
2018-01-26 22:52:05 +0100

    project commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace minesweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xxx = "sfksfjk";
+            Console.WriteLine(xxx);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+simple C# hello world
