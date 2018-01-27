0f70ef4570b2bebc903afc6ad725f424772b73c9
Martin Hanyáš <28785549+m-handy@users.noreply.github.com>
2018-01-27 15:55:35 +0100

    split dictionaries for mines and rest

--- minesweeper/src/Playground.cs
+++ minesweeper/src/Playground.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace minesweeper
 {
@@ -11,6 +13,9 @@ namespace minesweeper
         private int minesCout;
         private int playgroundSize;
         private Dictionary<Point, Field> playground = new Dictionary<Point, Field>();
+        private Dictionary<Point, Field> onlyMines = new Dictionary<Point, Field>();
+        
+
 
         public Playground(int rows, int columns, int minesCount)
         {
@@ -26,6 +31,14 @@ namespace minesweeper
 
             LayMines();
             FillRestPlayground();
+            Stopwatch watch = Stopwatch.StartNew();
+            foreach (var xxx in onlyMines)
+            {
+                playground.Add(xxx.Key, xxx.Value);
+            }
+            //Parallel.ForEach(onlyMines, (xxx) => { playground.Add(xxx.Key, xxx.Value); });
+            watch.Stop();
+            Console.WriteLine("Merge Dict: {0}ms", watch.ElapsedMilliseconds);
         }
 
         public void Print()
@@ -46,7 +59,7 @@ namespace minesweeper
 
         private bool IsMine(Point point)
         {
-            return playground.ContainsKey(point) && playground[point] is Mine;
+            return onlyMines.ContainsKey(point);// && playground[point] is Mine;
         }
 
         private int GetValue(Point point)
@@ -133,7 +146,7 @@ namespace minesweeper
 
         private void CreateField(Point point)
         {
-            if (!playground.ContainsKey(point))
+            if (!onlyMines.ContainsKey(point))
             {
                 var pole = new Field(point, GetValue(point));
                 playground.Add(pole.Point, pole);
@@ -172,10 +185,10 @@ namespace minesweeper
                 do
                 {
                     minePosition = GetCoordinates(rnd.Next(0, playgroundSize));
-                } while (playground.ContainsKey(minePosition));
+                } while (onlyMines.ContainsKey(minePosition));
 
                 var mine = new Mine(minePosition);
-                playground.Add(mine.Point, mine);
+                onlyMines.Add(mine.Point, mine);
             }
             watch.Stop();
             Console.WriteLine("LayMines: {0}ms", watch.ElapsedMilliseconds);
